60df0e754b93f4f079ec70c78028796174d1723f
Robert <robert.holl@gmail.com>
2017-04-02 22:32:54 -0400

    Modified backup script

---
+++ backupscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+RPATH="/practice"
+RSOURCE="/source"
+ARCHIVE_FILE="archive-`date +"%Y%m%d"`.tar.gz"
+
+if [ ! -d $HOME"$RPATH" ]; then
+	mkdir $HOME"$RPATH"
+	mkdir $HOME"$RPATH"/newlocation
+	echo "Created folder"
+else
+	echo "Folder already exists"
+fi
+
+tar -czf $HOME"$RPATH"/newlocation/$ARCHIVE_FILE --directory=$HOME"$RSOURCE/" .
+
+
+#echo "Before untar"
+#tar -xvf $HOME"$RPATH"/newlocation/$ARCHIVE_FILE
+
+
+
+
